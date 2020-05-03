c50c1a0677d12e32ba5f6747b216434930da972c
OtacilioN <contato@otaciliomaia.com>
2020-05-03 02:31:44 -0300

    Add Example

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,36 @@
 import React from 'react'
+import RFor from 'react-render-for'
 
-import { ExampleComponent } from 'react-render-for'
-import 'react-render-for/dist/index.css'
+const data = [
+  'Elemento 1',
+  'Elemento 2',
+  'Elemento 3',
+  'Elemento 4',
+  'Elemento 5',
+  'Elemento 6',
+  'Elemento 7',
+  'Elemento 8',
+  'Elemento 9',
+  'Elemento 10',
+  'Elemento 11',
+  'Elemento 12',
+  'Elemento 13',
+  'Elemento 14',
+  'Elemento 15'
+]
+
+const Box = ({ item }) => (
+  <div className='BoxStyle'>
+    <h6>{item}</h6>
+  </div>
+)
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <div className='AppWrapper'>
+      <RFor data={data} renderItem={(item) => <Box item={item} />} />
+    </div>
+  )
 }
 
 export default App
